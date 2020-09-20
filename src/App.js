5f43a4e18a0fb69593bfada30a6232b7a7f14103
laurentravix <laurentravix@gmail.com>
2020-09-20 11:30:25 +0200

    Adding Components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [inputText, setInputText] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>Lenny's Todo List</header>
+      <Form setInputText={setInputText}/>
+      <TodoList />
     </div>
   );
 }
